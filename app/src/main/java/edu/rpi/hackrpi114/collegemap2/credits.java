235fcddf5296b0922ac16fea8d1d0e4c2d4a876f
s-thakur <samvit16@gmail.com>
2016-11-12 16:18:00 -0500

    hello

---
+++ app/src/main/java/edu/rpi/hackrpi114/collegemap2/credits.java
@@ -0,0 +1,13 @@
+package edu.rpi.hackrpi114.collegemap2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class credits extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.credits);
+    }
+}
